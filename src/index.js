c733e25c4d4a41e315107304e6a4ef237e8c59fe
Yusuf Eka Wicaksana <33868220+WicaksanaYusuf@users.noreply.github.com>
2021-02-28 16:52:41 +0700

    part1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+const Hello = (props) => {
+  return (
+    <div>
+      <p>Hello world, {props.name } and age {props.age}</p>
+    </div>
+  )
+}
+
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by <a href="https://github.com/mluukkai">mluukkai</a>
+    </div>
+  )
+}
+
+
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+   return (
+    <>
+      <p>Greeting</p>
+      <Hello name="Yusuf" age={24 + 10}/>
+      <Hello name={name} age={age}/>
+
+      <Footer/>
+
+    </>
+   )
+}
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,4 +46,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
