e6d353844e529dbf6be438bb9a9a159a4b4a735f
ysfwicaksana <ekayusuf.wicaksana@gmail.com>
2021-03-01 20:55:26 +0700

    part1 end

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+
+const Button = (props) => {
+  return <button onClick={props.handleClick}>{props.text}</button>;
+};
+
+const Result = ({ text, count }) => {
+  if (count) {
+    return (
+      <>
+        <p>
+          {text} {count}
+        </p>
+      </>
+    );
+  }
+
+  return <>No feedback given</>;
+};
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+
+  const handleGood = () => setGood(good + 1);
+  const handleBad = () => setBad(bad + 1);
+  const handleNeutral = () => setNeutral(neutral + 1);
+
+  const handleAverage = () => {
+    let defaultGood = 1 * good;
+    let defaultNeutral = 0 * neutral;
+    let defaultBad = -1 * bad;
+
+    return defaultGood + defaultNeutral + defaultBad;
+
+    // console.log(result);
+  };
+
+  return (
+    <>
+      <h1>Give Feedback</h1>
+      <Button handleClick={handleGood} text="good" />
+      <Button handleClick={handleNeutral} text="neutral" />
+      <Button handleClick={handleBad} text="bad" />
+      <h1>Statistics</h1>
+      <Result text="good" count={good} />
+      <Result text="neutral" count={neutral} />
+      <Result text="bad" count={bad} />
+      <Result text="all" count={good + bad + neutral} />
+      {/* <Result text="average" count={handleAverage} /> */}
+    </>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,46 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-// import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 // import reportWebVitals from './reportWebVitals';
-
-const Hello = (props) => {
-  return (
-    <div>
-      <p>Hello world, {props.name } and age {props.age}</p>
-    </div>
-  )
-}
-
-const Footer = () => {
-  return (
-    <div>
-      greeting app created by <a href="https://github.com/mluukkai">mluukkai</a>
-    </div>
-  )
-}
-
-
-const App = () => {
-  const name = 'Peter'
-  const age = 10
-   return (
-    <>
-      <p>Greeting</p>
-      <Hello name="Yusuf" age={24 + 10}/>
-      <Hello name={name} age={age}/>
-
-      <Footer/>
-
-    </>
-   )
-}
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
